e75fb33c0f583aa35040a5131d02af3f68d1c7a4
Maksim <github073108@gmail.com>
2026-03-31 16:47:18 +0300

    Create matrix A(html + css)

--- matrix/views.py
+++ matrix/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Matrix',
-        'cells': [
-            'Cell1',
-            'Cell2',
-        ]
+        'matrix': 'matrix',
+        'rows': range(4),
+        'columns': range(5),
     }
     return render(request, 'matrix/index.html', context)
