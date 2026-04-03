da7dd72d389dc5b10b6d70b2e84ad6de027c6e41
Maksim <github073108@gmail.com>
2026-04-03 16:57:24 +0300

    Create model Article

--- matrix/models.py
+++ matrix/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-created_at']
+
+   
